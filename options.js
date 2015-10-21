fefd6aa10b77a71e51f969484738a51e9d252840
William Dutton <will.dutt@gmail.com>
2015-10-21 15:48:35 +1000

    make options work again

--- options.js
+++ options.js
@@ -1,14 +1,15 @@
 function init(){
-       var bg = chrome.extension.getBackgroundPage().settings;
-       $("#jiraBaseUrl").val(bg.get('jiraBaseUrl'));
+	$('#jiraBaseUrl').on("keypress", function(e) {if (e.keyCode == 13) {save()}});
 
-       $('#jiraBaseUrl').on("keypress", function(e) {if (e.keyCode == 13) {save()}});
-       $('#note').click(function(e){$("#jiraBaseUrl").val("https://jira.atlassian.com")});
+	var bg = chrome.extension.getBackgroundPage().settings;
+	$("#jiraBaseUrl").val(bg.get('jiraBaseUrl'));
+
+	$('#note').click(function(e){$("#jiraBaseUrl").val("https://jira.atlassian.com")});
 }
 function save(){
-       var bgSettings = chrome.extension.getBackgroundPage().settings;
-       bgSettings.set('jiraBaseUrl',jQuery('#jiraBaseUrl').val());
+	var bgSettings = chrome.extension.getBackgroundPage().settings;
+	bgSettings.set('jiraBaseUrl',jQuery('#jiraBaseUrl').val());
 }
-
+		
 window.addEventListener("load", init);
 document.getElementById("save").addEventListener("click",save);
